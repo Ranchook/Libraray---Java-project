f052f0468cb44331df09b13b6ff97f9dcf1c9461
Ran Shneor <102590409+Ranchook@users.noreply.github.com>
2024-06-30 13:53:46 +0300

    Update Book.java

--- classes/Book.java
+++ classes/Book.java
@@ -1,11 +1,8 @@
 package classes;
 
-
 import java.io.Serializable;
 
-
 public class Book implements Serializable {
-
 
 	private static final long serialVersionUID = 1L;
 	private String book_name;
